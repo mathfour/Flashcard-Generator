cc919d60386eaf15b4c88f2e93b50ef94cb855f9
Bon Crowder <SilverDash@Bons-MacBook-Pro.local>
2017-05-20 12:39:40 -0500

    save-flashcards.js has all the info to save both types of flashcards now

--- flashcard.js
+++ flashcard.js
@@ -1,23 +1,63 @@
-
+// bmc: todo print the code and figure out what's going on
+// bmc: todo map out the details on what I have and what I still need
+var inquirer = require('inquirer');
 var fs = require("fs");
 
-function BasicFlashcard(){
-    this.question = question;
-    this.answer = answer;
+inquirer.prompt(
+    {
+        type: "list",
+        message: "Do you want to create flashcards or study?",
+        name: "createOrStudy",
+        choices: ["Create a Flashcard", "Study from Flashcards"]
+    }).then(function (answers) {
+
+    if (answers.createOrStudy === "Create a Flashcard") {
+        action = "create";
+        console.log("You're creating a flashcard");
+        // bmc: launch the function to prompt for type of flashcard
+        getCardType(handleTheCard, action);
+    }
+    else if (answers.createOrStudy === "Study from Flashcards") {
+        action = "read";
+        console.log("You're viewing a flashcard");
+        // bmc: launch the function to view a random flashcard (or ask which kind they want)
+        getCardType(handleTheCard, action);
+    }
+});
+
+function Flashcard(front, back, type){
+    this.front = front;
+    this.back = back;
+    this.type = type;
+    this.showFront = function (front) {
+        console.log("------------------------------------------");
+        console.log("|                                        |");
+        console.log("|                                        |");
+        console.log("|                                        |");
+        console.log(""
+        console.log("|                                        |");
+        console.log("|                                        |");
+        console.log("|                                        |");
+        console.log("------------------------------------------");
+    }
 }
 
-function ClozeFlashcard() {
-    this.statement = statement;
-    this.deletion = deletion;
-}
+Flashcard.prototype.showFront = function () {
 
-BasicFlashcard.prototype.showQandA = function () {
-    console.log(this.question + " " + this.answer);
+    console.log(this.front);
 };
 
-ClozeFlashcard.prototype.showPartialText = function () {
-    // bmc: find the deletion in the statement and suck it out
-};
+// BasicFlashcard.prototype.showQandA = function () {
+//     console.log(this.question + " " + this.answer);
+// };
+//
+// BasicFlashcard.prototype.showQuestion = function () {
+//     console.log(this.question);
+// }
+//
+// BasicFlashcard.prototype.showAnswer = function () {
+//     console.log(this.answer);
+// }
 
 BasicFlashcard.prototype.saveQandA = function () {
  fs.appendFile("basic-cards.txt", this.question + " " + this.answer, function(err) {
@@ -25,10 +65,23 @@ BasicFlashcard.prototype.saveQandA = function () {
     console.log(err);
   }
   else {
+      var flashcard = new Flashcard()
     console.log("Your flashcard has been saved!");
   }
 });
 };
+//
+// ClozeFlashcard.prototype.showPartialText = function () {
+//     // bmc: find the deletion in the statement and suck it out
+// };
+//
+// ClozeFlashcard.prototype.showClozeDeletion = function () {
+//     console.log(this.deletion);
+// }
+//
+// ClozeFlashcard.prototype.showFullText = function () {
+//     console.log(this.statement);
+// }
 
 ClozeFlashcard.prototype.saveFullText = function () {
    fs.appendFile("cloze-cards.txt", this.statement, function (err) {
@@ -39,4 +92,37 @@ ClozeFlashcard.prototype.saveFullText = function () {
           console.log("Your flashcard has been saved!");
       }
    })
+}
+
+function getCardType(handleTheCard) {
+    inquirer.prompt({
+        type: "list",
+        message: "Q & A cards or Fill-in-the-blank?",
+        name: "cardType",
+        choices: ["Q & A", "Fill-in-the-blank"]
+    }).then(function (answers) {
+        if (answers.cardType === "Q & A") {
+            console.log("you picked QA");
+            cardType = "questionAnswer";
+            questionAnswer(action);
+        }
+        else if (answers.cardType === "Fill-in-the-blank") {
+            console.log("you picked cloze");
+            cardType = "clozeDeleted";
+            clozeDeleted(action);
+        }
+        console.log(cardType);
+    });
+    handleTheCard(action, cardType);
+}
+
+function handleTheCard(action, cardType) {
+   if (action === "read"){
+       // bmc: read the card
+       console.log("you want to read the", cardType, "card.");
+   }
+   else if (action === "create") {
+       // bmc: create the card
+       console.log("you want to create the", cardType, "card.");
+   }
 }
---
+++ testing.js
@@ -0,0 +1,15 @@
+/**
+ * Created by SilverDash on 5/19/17.
+ */
+
+function add (a, b) {
+    c = a + b;
+    return c;
+}
+
+function multiply (a, b) {
+    c = a * b;
+    return c;
+}
+console.log(add(5, 7));
+console.log(multiply(2,6));
